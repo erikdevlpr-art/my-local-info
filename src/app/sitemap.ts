574a37f73895fe08bf0f4699dfb6993f47432a52
Erik Dev <erik.devlpr@gmail.com>
2026-06-11 00:35:24 +0300

    빌드 에러 수정: robots.ts 및 sitemap.ts에 force-static 설정 적용

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { getSortedPostsData } from '../lib/posts';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://my-local-info-5c6.pages.dev';
 
